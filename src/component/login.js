616b41e9ffdd7c6d7a30d77fb6b95f6b23dc25ee
Maria Bolaños <mariadlp@gmail.com>
2019-08-05 16:55:19 -0500

    Local storage stores token

--- src/component/login.js
+++ src/component/login.js
@@ -7,69 +7,61 @@ const Login = props => {
   const [password, setPassword] = useState("");
 
   const handleChange = (event) => {
-        if (event.target.name === "email") {
-            setEmail(event.target.value);
-        }
-        if (event.target.name === "password") {
-            setPassword(event.target.value);
-        }
+		if (event.target.name === "email") {
+			setEmail(event.target.value);
+		}
+		if (event.target.name === "password") {
+			setPassword(event.target.value);
+		}
 
-        console.log(email);
-        console.log(password);
-    };
+		console.log(email);
+		console.log(password);
+	};
 
-       function loging() {
-      // try {
-         fetch('/api/user/login',
-               {
-                   method: 'POST',
-                   headers: { 'Content-Type': 'application/json' },
-                   body: JSON.stringify({ email, password })
-               }
-           ).then((response) => {
+		function login() {
 
-             return response.json();
+		 fetch('/api/user/login',
+			   {
+				   method: 'POST',
+				   headers: { 'Content-Type': 'application/json' },
+				   body: JSON.stringify({ email, password })
+			   }
+		   ).then((response) => {
 
-           }).then((response) => {
-             console.log(response);
-             localStorage.setItem("token", JSON.stringify({token: response}));
-           })
-          //  console.log(token)
-    //   } catch (err) {
-        //   console.log(err);
-      // }
-   };
+			 return response.json();
+
+		   }).then((response) => {
+			 console.log(response);
+			 localStorage.setItem("token", JSON.stringify({token: response}));
+			})
+
+	};
 
 
+	return(
 
-   console.log(loging);
-  return(
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Correo electrónico</Form.Label>
-        <Form.Control  onChange={handleChange} type="email" name="email" placeholder="Ingresa tu correo" />
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control onChange={handleChange} type="password" name="password" placeholder="Ingresa tu contraseña" />
-      </Form.Group>
-      <Button onClick={loging} variant="primary">
-        Ingresar
-      </Button>
+	<Form>
+		<Form.Group controlId="formBasicEmail">
+			<Form.Label>Correo electrónico</Form.Label>
+			<Form.Control  onChange={handleChange} type="email" name="email" placeholder="Ingresa tu correo" />
+		</Form.Group>
+		<Form.Group controlId="formBasicPassword">
+			<Form.Label>Contraseña</Form.Label>
+			<Form.Control onChange={handleChange} type="password" name="password" placeholder="Ingresa tu contraseña" />
+		</Form.Group>
+		<Button onClick={login} variant="primary">
+		I	ngresar
+		</Button>
 
-      <Button className="form-group text-center">
-                           <Link to="/Register">
-                                   No tienes una cuenta
-                                   ¡Registrate!
+		<Button className="form-group text-center">
+		<Link to="/Register">
+				No tienes una cuenta
+				¡Registrate!
+		</Link>
+		</Button>/Button>
+	</Form>
 
-                           </Link>
-                       </Button>
-    </Form>
-
-
-
-  )
-
+	)
 }
 
 
