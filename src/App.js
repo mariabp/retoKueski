4bb88dd3c53f5b8bcf88ffa8eccec2d3d749513a
Perla <pataditaenloshuevos@gmail.com>
2019-08-05 12:02:11 -0500

    create login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Login from './component/login';
+import {HashRouter, Route, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      Holi
-    </div>
-  );
+const App = ()=>{
+
+  return(
+    <HashRouter basename="/">
+       <div className="App">
+         <Switch>
+           <Route exact path="/" render={() => <Login />}/>
+         </Switch>
+       </div>
+     </HashRouter>
+  )
 }
 
 export default App;
