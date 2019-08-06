ff9dfbc48cc29be0a3a388d00692a9b723088f04
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-08-05 20:34:00 -0500

    Solved conflicts

--- src/component/login.js
+++ src/component/login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React , { useState } from 'react';
+import { Form , Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
 import styled from 'styled-components';
