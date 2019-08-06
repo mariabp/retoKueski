ac3ef3422df4156b342e9f764f3414f11498ef3d
Perla <pataditaenloshuevos@gmail.com>
2019-08-06 11:04:15 -0500

    grafica lista

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-import DisplayGoals from './displayGoals'
+import DisplayGoals from './displayGoals';
+
 
 const Home = props => {
 
