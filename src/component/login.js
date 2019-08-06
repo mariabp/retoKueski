4330ea72b230fe6f186e4b1503188f4ff7591349
angie <angyalva16@gmail.com>
2019-08-05 22:00:50 -0500

    Added home component

--- src/component/login.js
+++ src/component/login.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {Form, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
 const Login = props => {
  const [email, setEmail] = useState('');
  const [password, setPassword] = useState('');
@@ -11,8 +13,6 @@ const Login = props => {
         if (event.target.name === 'password') {
             setPassword(event.target.value);
         }
-        console.log(email);
-        console.log(password);
     };
         function login() {
          fetch('/api/user/login',
@@ -26,7 +26,8 @@ const Login = props => {
            })
            .then((response) => {
              console.log("sesion activa");
-             localStorage.setItem('token', JSON.stringify({token: response}));
+             localStorage.setItem('token', JSON.stringify(response));
+             props.history.push('/Home')
             })
     };
     return(
@@ -39,12 +40,9 @@ const Login = props => {
             <Form.Label>Contraseña</Form.Label>
             <Form.Control onChange={handleChange} type='password' name='password' placeholder='Ingresa tu contraseña' />
         </Form.Group>
-        <Link to='/Home'>
         <Button onClick={login} variant='primary'>
         Ingresar
         </Button>
-        </Link>
-        
         <Button className='form-group text-center'>
         <Link to='/Register'>
                 No tienes una cuenta
@@ -54,4 +52,4 @@ const Login = props => {
     </Form>
     )
 }
-export default Login;
+export default withRouter(Login);
