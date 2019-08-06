be899f2954b08e6c51e2106c2d7348d9676ea603
angie <angyalva16@gmail.com>
2019-08-06 08:54:59 -0500

    Change the component displayGoals to React Boostrap

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-import DisplayGoals from './DisplayGoals'
+import DisplayGoals from './displayGoals'
 
 const Home = props => {
 
